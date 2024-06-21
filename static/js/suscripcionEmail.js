56af76b2e914e4a85ee65cba8af3ff028e054300
elmerurbina <[elmerurbina570@gmail.com]>
2024-06-21 09:37:19 -0600

    updates, logo added

---
+++ static/js/suscripcionEmail.js
@@ -0,0 +1,21 @@
+document.getElementById('subscribeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var emailInput = document.getElementById('email');
+    var message = document.getElementById('message');
+
+    if (validateEmail(emailInput.value)) {
+        message.textContent = 'Suscripción realizada con éxito';
+        message.style.color = 'green';
+    } else {
+        message.textContent = 'Por favor, ingresa un correo electrónico válido';
+        message.style.color = 'red';
+    }
+
+    message.style.display = 'block';
+});
+
+function validateEmail(email) {
+    var re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return re.test(String(email).toLowerCase());
+}
