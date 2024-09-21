8f5652e3fc268f5eb688615c69d3c615e96c59ce
elmerurbina <[elmerurbina570@gmail.com]>
2024-09-21 10:23:08 -0600

    Se agrego un Menu de barras en todas las interfaces

--- static/js/campaniasForm.js
+++ static/js/campaniasForm.js
@@ -27,3 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
         return isValid;
     }
 });
+
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    const menu = document.getElementById('menu-items');
+    menu.classList.toggle('hidden');
+});
